7e05fc7beadee1239358b4fcc4cf61ec01aa8cd6
Carolin Moellercat-miller <github@carolinmoeller.com>
2022-04-23 15:17:38 +0200

    Fetch only one random character from api

--- js/main.js
+++ js/main.js
@@ -1,26 +1,30 @@
 // import { elements } from './selectors.js';
 // import fetchData from './fetch.js';
 
-let characterData = [];
+let characterName = [];
+let characterImage = [];
 const cardContainer = document.querySelector('[data-js="cardContainer"]');
-let pageNumber = getRandomPage(1, 42);
+
+let randomCharacter = getRandomCharacter(1,826);
+
 const button = document.querySelector('[data-js="button"]');
 
 function fetchDataAndRender() {
-  fetch(`https://rickandmortyapi.com/api/character/?page=${pageNumber}`)
+  fetch(`https://rickandmortyapi.com/api/character/${randomCharacter}`)
     .then(response => response.json())
     .then(data => {
-      console.log(data);
-      characterData = data.results;
-      characterData.forEach(character => {
-        const item = createCharacterCard(character);
+      
+      characterImage = data.image;
+      characterName = data.name;
+        const item = createCharacterCard();
         cardContainer.appendChild(item);
-      });
+      
     });
 }
 
-function getRandomPage(min, max) {
+function getRandomCharacter(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
+
 }
 
 button.addEventListener('click', () => {
@@ -28,12 +32,13 @@ button.addEventListener('click', () => {
   fetchDataAndRender();
 });
 
-function createCharacterCard(characterData) {
+function createCharacterCard() {
+  
   const card = document.createElement('div');
   card.classList.add('card__element');
   card.innerHTML = `
-  <img src="${characterData.image}" alt="${characterData.name}"></img>
-  <h3>${characterData.name}</h3>
+  <img src="${characterImage}" alt="${characterName}"></img>
+  <h3>${characterName}</h3>
   `;
   return card;
 }
